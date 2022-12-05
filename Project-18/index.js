d31aa92f8386113f919369bfa6eefa9f8c5e930c
Alexander Alves <vendas77.rj@gmail.com>
2022-12-05 17:47:33 -0300

    Finalizing the style file

--- Project-18/index.js
+++ Project-18/index.js
@@ -1,3 +1,4 @@
-function videoUrl(info) {
-  document.getElementById("slider").src = info
-}
+window.addEventListener("scroll", function() {
+  let header = document.querySelector("header")
+  header.classList.toggle("sticky", window.scrollY > 0)
+})
