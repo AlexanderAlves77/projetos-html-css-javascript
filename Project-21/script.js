c2a7902ca49a2793cc2a948a16ba22022f9246d4
Alexander Alves <vendas77.rj@gmail.com>
2022-12-08 19:29:08 -0300

    Finalizing the project

--- Project-21/script.js
+++ Project-21/script.js
@@ -1,9 +1,18 @@
 document.addEventListener("mousemove" , (evt) => {
   let body = document.querySelector("body")
-  let heart = document.querySelector("span")
+  let heart = document.createElement("span")
   let x = evt.offsetX
   let y = evt.offsetY
+
   heart.style.left = `${x}px`
   heart.style.top = `${y}px`
+  let size = Math.random() * 100
+
+  heart.style.width = 20 + size + 'px'
+  heart.style.height = 20 + size + 'px'
   body.appendChild(heart)
+
+  setTimeout(() => {
+    heart.remove()
+  }, 2000)
 })
