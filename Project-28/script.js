ccdb6c1ded54e98c25b70084deb0c10669753a2e
Alexander Alves <vendas77.rj@gmail.com>
2022-12-26 18:58:55 -0300

    Create the file js

---
+++ Project-28/script.js
@@ -0,0 +1,14 @@
+const clock = () => {
+  const hours = document.getElementById("hour")
+  const minutes = document.getElementById("minute")
+  const seconds = document.getElementById("second")
+  const h = new Date().getHours()
+  const m = new Date().getMinutes()
+  const s = new Date().getSeconds()
+
+  hours.innerHTML = h 
+  minutes.innerHTML = m 
+  seconds.innerHTML = s 
+}
+
+let interval = setInterval(clock, 1000)
