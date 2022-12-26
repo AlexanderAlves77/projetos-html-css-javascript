90b9be90d86948e7af4c345f58ac4f15d1f07c45
Alexander Alves <vendas77.rj@gmail.com>
2022-12-26 19:10:26 -0300

    Finish the project

--- Project-28/script.js
+++ Project-28/script.js
@@ -2,13 +2,22 @@ const clock = () => {
   const hours = document.getElementById("hour")
   const minutes = document.getElementById("minute")
   const seconds = document.getElementById("second")
-  const h = new Date().getHours()
-  const m = new Date().getMinutes()
-  const s = new Date().getSeconds()
+  const ampm = document.getElementById("ampm")
+  let h = new Date().getHours()
+  let m = new Date().getMinutes()
+  let s = new Date().getSeconds()
+  let am = "AM"
+
+  if (h > 12) am = "PM"
+
+  h = (h < 10) ? `0${h}` : h
+  m = (m < 10) ? `0${m}` : m
+  s = (s < 10) ? `0${s}` : s
 
   hours.innerHTML = h 
   minutes.innerHTML = m 
   seconds.innerHTML = s 
+  ampm.innerHTML = am
 }
 
 let interval = setInterval(clock, 1000)
