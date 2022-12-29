a522f2033e960ba8b5288e11b1c5b24a12308b72
Alexander Alves <vendas77.rj@gmail.com>
2022-12-29 20:15:21 -0300

    Finish the project

---
+++ Project-29/script.js
@@ -0,0 +1,7 @@
+const toggle = () => {
+  let blur = document.getElementById("blur")
+  blur.classList.toggle('active')
+  
+  let popup = document.getElementById("popup")
+  popup.classList.toggle('active')
+}
