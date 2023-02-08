bed3dd467d36438b685d8bd159e7a2a300d62976
Alexander Alves <vendas77.rj@gmail.com>
2023-02-08 19:16:08 -0300

    Finalizing the project

--- Project-33/script.js
+++ Project-33/script.js
@@ -1,1 +1,22 @@
-/* <span>0</span>
+let numbers = document.getElementById('box')
+
+for (i = 0; i < 100; i++) {
+  let span = document.createElement('span')
+  span.textContent = i 
+  numbers.appendChild(span)
+}
+
+let num = numbers.getElementsByTagName('span')
+let index = 0 
+
+const nextNum = () => {
+  num[index].style.display = 'none'
+  index = (index + 1) % num.length 
+  num[index].style.display = 'initial'
+}
+
+const prevNum = () => {
+  num[index].style.display = 'none'
+  index = (index - 1 + num.length) % num.length 
+  num[index].style.display = 'initial'
+}
